90115cfc6dd766dc8e244f661fb8170bf1ff08b7
Krishna Chaitanya <kchaitanya556@gmail.com>
2021-02-20 23:38:52 +0530

    Add Helper scripts folder. Move ipynbs to Notebooks folder. Add requirements.txt

---
+++ Helpers/__init__.py
@@ -0,0 +1,3 @@
+from Helpers import *
+
+__all__ = ["value_counts_for_multiple_cat_cols"]
--- Helpers/value_counts_for_multiple_cat_cols.py
+++ Helpers/value_counts_for_multiple_cat_cols.py
@@ -1,11 +1,26 @@
 class ValueCounts:
 
-    def __init__(self, columns):
+    def __init__(self, columns, df):
         self.columns = columns
+        self.df = df
 
     def valcounts_multiple_cat_cols(self):
         """
-        Returns value counts for multiple
-        """        
-        for col in columns:
-            col.value_counts()
+        Returns value counts for multiple.
+
+        :param columns: Categorical columns dataframe
+        :type columns: DataFrame
+        :param df: Main DataFrame
+        :type df: DataFrame
+
+        :return None:
+        """
+
+        assert len(self.columns) != 0
+
+        for col in self.columns:
+            print(f"Value Counts for {col}")
+            print("\n")
+            print(self.df[col].value_counts())
+            print("---------------------")
+        
